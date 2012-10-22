cf342543f86d4ce73fba2f7c2c2e3467555bf982
stefanbehr <sbehr1@gmail.com>
2012-10-22 16:36:28 -0700

    cleaning up the mess

--- Viterbi.py
+++ Viterbi.py
@@ -8,10 +8,10 @@ class Viterbi:
 			stdin=PIPE, stdout=PIPE)
 
 	def tag(self, input):
-		self.process.stdin.write(input + '\n')
+		self.process.stdin.write(input)
 		self.process.stdin.flush()
 		return self.process.stdout.readline()
 
-v = Viterbi()
-print v.tag('this is a test .')
-print v.tag('that is a test .')
+if __name__ == '__main__':
+        v = Viterbi()
+        print v.tag('this is a test .')
